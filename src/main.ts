b99c4c55d634c3a65e5c405210e03be1672d2136
thales <thales.sousufpi@gmail.com>
2024-07-15 14:20:20 -0300

    feat: create swagger bearer auth

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,21 @@ async function bootstrap() {
     .setDescription('API used for testing purpose')
     .setVersion('1.0.0')
     .setBasePath('api')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'Bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+      },
+      'token',
+    )
     .addSecurityRequirements('token')
     .build();
   const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.LOCAL_HOST || 3000);
 }
 bootstrap();
